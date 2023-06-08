04682787569ceb201be2de5d8b5db789c8a7e7ce
Cezary Babik <babik.czarek@gmail.com>
2023-06-08 20:05:01 +0200

    1

--- src/MeczLiny.java
+++ src/MeczLiny.java
@@ -1,12 +1,36 @@
 package src;
-import java.util.Random;
 
 public class MeczLiny extends Mecz {
     public MeczLiny(Sedzia Sedzia, Druzyna druzyna1, Druzyna druzyna2){
         this.Sedzia = Sedzia;
         this.druzyna1 = druzyna1;
         this.druzyna2 = druzyna2;
+
+
+
+        int sumaS1 = obliczSumeSily(druzyna1);
+        int sumaS2 = obliczSumeSily(druzyna2);
+
+        int sumaM1 = obliczSumeMasy(druzyna1);
+        int sumaM2 = obliczSumeMasy(druzyna2);
     }
+    private int obliczSumeSily(Druzyna druzyna){
+        int suma = 0;
+        for (int i = 0; i < 6; i++) {
+            suma += druzyna.getRep(i).getSila();
+        }
+        return suma;
+    }
+
+    private int obliczSumeMasy(Druzyna druzyna) {
+        int suma = 0;
+        for (int i = 0; i < 6; i++) {
+            suma += druzyna.getRep(i).getWaga();
+        }
+        return suma;
+    }
+
+
 
     @Override
     public String toString(){
