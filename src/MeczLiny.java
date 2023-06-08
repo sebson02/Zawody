f57dd153d4e5615db265ab20a234b977c1738668
Cezary Babik <babik.czarek@gmail.com>
2023-06-08 20:16:17 +0200

    1

--- src/MeczLiny.java
+++ src/MeczLiny.java
@@ -1,18 +1,29 @@
 package src;
 
+import java.util.Random;
+
 public class MeczLiny extends Mecz {
     public MeczLiny(Sedzia Sedzia, Druzyna druzyna1, Druzyna druzyna2){
         this.Sedzia = Sedzia;
         this.druzyna1 = druzyna1;
         this.druzyna2 = druzyna2;
+        Random x = new Random();
 
-
+        int luck1 = x.nextInt(80,101);
+        int luck2 = x.nextInt(80,101);
 
         int sumaS1 = obliczSumeSily(druzyna1);
         int sumaS2 = obliczSumeSily(druzyna2);
 
         int sumaM1 = obliczSumeMasy(druzyna1);
         int sumaM2 = obliczSumeMasy(druzyna2);
+
+        for (int i = 0; i < 6; i++){
+            druzyna1.getRep(i).zagral();
+        }
+        for (int j = 0; j < 6; j++){
+            druzyna2.getRep(j).zagral();
+        }
     }
     private int obliczSumeSily(Druzyna druzyna){
         int suma = 0;
