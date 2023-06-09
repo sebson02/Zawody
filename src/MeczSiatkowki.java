5e4a559b8b179f4532d956a2f37aaba046f2974a
Cezary Babik <babik.czarek@gmail.com>
2023-06-09 17:17:18 +0200

    1

--- src/MeczSiatkowki.java
+++ src/MeczSiatkowki.java
@@ -1,4 +1,11 @@
 package src;
 
-public class MeczSiatkowki {
+import java.io.Serializable;
+
+public class MeczSiatkowki extends Mecz implements Serializable {
+
+    public MeczSiatkowki(Sedzia sedzia, Sedziapom sedziapom1, Sedziapom sedziapom2, Druzyna druzyna1, Druzyna druzyna2){
+
+
+    }
 }
