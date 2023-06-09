9677894454c7255ee6e0573abb92c500d6ecfc3d
sebson02 <112127651+sebson02@users.noreply.github.com>
2023-06-09 16:01:59 +0200

    Update BazaImion.java
    zmieniłem klase jak rabin przykazał

--- src/BazaImion.java
+++ src/BazaImion.java
@@ -1,71 +1,50 @@
 package src;
 
 public class BazaImion {
-    private static String im1 = "Szymuś";
-    private static String im2 = "Czaro";
-    private static String im3 = "Bartek";
-    private static String im4 = "Dżejkob";
-    private static String im5 = "Dario";
-    private static String im6 = "Dzbanusz";
-    private static String im7 = "Bożydar";
-    private static String im8 = "Franio";
-    private static String im9 = "Marcinek";
-    private static String im10 = "Ełgeniusz";
-    private static String im11 = "Marek";
-    private static String im12 = "Łukasz";
-    private static String im13 = "Sebson";
-    private static String im14 = "Robercik";
-    private static String im15 = "Karol";
-    private static String im16 = "Gerwazy";
-    private static String im17 = "Dżon";
-    private static String im18 = "Wiktor";
-    private static String im19 = "Szon";
-    private static String im20 = "Wojtunio";
-
     public static String wybierz(int a) {
         switch (a) {
             case 1:
-                return im1;
+                return Szymuś;
             case 2:
-                return im2;
+                return Czaro;
             case 3:
-                return im3;
+                return Bartek;
             case 4:
-                return im4;
+                return Dżejkob;
             case 5:
-                return im5;
+                return Dario;
             case 6:
-                return im6;
+                return Dzbanusz;
             case 7:
-                return im7;
+                return Bożydar;
             case 8:
-                return im8;
+                return Franio;
             case 9:
-                return im9;
+                return Marcinek;
             case 10:
-                return im10;
+                return Ełgeniusz;
             case 11:
-                return im11;
+                return Marek;
             case 12:
-                return im12;
+                return Łukasz;
             case 13:
-                return im13;
+                return Sebson;
             case 14:
-                return im14;
+                return Robercik;
             case 15:
-                return im15;
+                return Karol;
             case 16:
-                return im16;
+                return Gerwazy;
             case 17:
-                return im17;
+                return Dżon;
             case 18:
-                return im18;
+                return Wiktor;
             case 19:
-                return im19;
+                return Szon;
             case 20:
-                return im20;
+                return Wojtunio;
             default:
-                return im1;
+                return Maciuś;
         }
     }
 }
